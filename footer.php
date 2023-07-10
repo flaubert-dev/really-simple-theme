304bfd335bfa3a739b9f116bba10feb386cbaa89
Flaubert Henriques <flaubertcontato@gmail.com>
2023-07-09 23:40:49 -0300

    develop: (update) footer.php

--- footer.php
+++ footer.php
@@ -40,8 +40,32 @@
 
   </footer><!-- .site-footer -->
 
+  <footer class="footer">
+    <small class="footer__copy">
+      &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>
+    </small>
+
+    <section class="footer__info">
+      <span class="footer__info-text">
+        <?php esc_html_e( 'Theme:', 'really-simple' ); ?>
+      </span>
+      
+      <a class="footer__info-link" href="<?php echo esc_url( 'https://wordpress.org/themes/really-simple/', 'really-simple' ); ?>" target="_blank" rel="noreferrer noopener">
+        <?php esc_html_e( 'Really Simple', 'really-simple' ); ?>
+      </a>
+
+      <span>/</span>
+
+      <span class="footer__info-text">
+        <?php esc_html_e( 'License:', 'really-simple' ); ?>
+      </span>
+
+      <a class="footer__info-link" href="<?php echo esc_url( 'https://www.gnu.org/licenses/gpl-3.0.html', 'really-simple' ); ?>" target="_blank" rel="noreferrer noopener">
+        <?php esc_html_e( 'GPLv3', 'really-simple' ); ?>
+      </a>
+    </section>
+  </footer>
+
   <?php wp_footer(); ?>
-
 </body>
-
 </html>

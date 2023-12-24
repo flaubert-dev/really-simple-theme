cdd105e7f3f975faf236c2ff11675e6a1317c0a9
Flaubert Henriques <flaubertcontato@gmail.com>
2023-12-24 12:06:22 -0300

    develop: (update) archive.php and description

--- archive.php
+++ archive.php
@@ -7,44 +7,4 @@
  * @package Really Simple
  */
 
-get_header(); ?>
-
-      <?php if ( have_posts() ) : ?>
-
-        <header class="page-header">
-          <?php
-            the_archive_title( '<h1 class="page-title">', '</h1>' );
-            the_archive_description( '<div class="archive-description">', '</div>' );
-          ?>
-        </header><!-- .page-header -->
-
-        <?php
-          // Start the Loop
-          while ( have_posts() ) : the_post();
-          
-            // Content
-            get_template_part( 'template-parts/content' );
-
-          endwhile; 
-
-          // Pagination
-          the_posts_navigation();
-
-      else:
-
-        // No content
-        get_template_part( 'template-parts/content', 'none' );
-
-      endif;  
-      ?>
-    
-    </main><!-- #main -->
-
-    <aside class="widget-area">
-      <?php get_sidebar(); ?>
-    </aside><!-- .widget-area -->
-
-  </div><!-- #primary -->
-
-<?php
-get_footer();
+?>
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -9,3 +9,20 @@
  */
 
 ?>
+
+<header class="page-header">
+  <?php
+    the_archive_title( '<h1 class="page-title">', '</h1>' );
+    the_archive_description( '<div class="archive-description">', '</div>' );
+  ?>
+</header>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php get_template_part( 'template-parts/content' ); ?>
+  
+  <?php endwhile; ?>
+  
+  <?php the_posts_navigation(); ?>
+<?php else : ?>
+  <?php get_template_part( 'template-parts/content', 'none' ); ?>
+<?php endif; ?>

1b77962f003791abdb18d1bcb66b50b1231f87c5
Flaubert Henriques <flaubertcontato@gmail.com>
2023-12-25 10:25:42 -0300

    develop: (update) search and searchform (.php)

--- searchform.php
+++ searchform.php
@@ -7,15 +7,25 @@
 
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
+<form 
+class="search-form" 
+method="get" 
+role="search" 
+action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-    <span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'really-simple' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search for:', 'placeholder', 'really-simple' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" />
+    <span class="screen-reader-text">
+			<?php esc_html_e( 'Search for:', 'really-simple' ); ?>
+		</span>
+
+		<input 
+		type="search" 
+		class="search-field" 
+		placeholder="<?php echo esc_attr_x( 'Search for:', 'placeholder', 'really-simple' ); ?>" 
+		value="<?php echo esc_attr( get_search_query() ); ?>" 
+		name="s" />
 	</label>
 
 	<button type="submit" class="search-submit">
     <?php echo esc_html_x( 'Search', 'submit button', 'really-simple' ); ?>
   </button>
-
 </form>
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,13 +10,27 @@
 
 ?>
 
-<?php echo '<p>' . esc_html__( 'No results were found, try searching for another word.', 'really-simple' ) . '</p>'; ?>
+<h1>
+  <?php 
+    /* translators: %s: search query. */
+    printf( esc_html__( 'Search Results for: %s', 'really-simple' ), '<span>' . get_search_query() . '</span>' );
+  ?>
+</h1>
 
-<?php 
-  get_search_form(); 
-  the_widget( 'WP_Widget_Recent_Posts', [
-    'title'   => esc_html__( 'Latest Posts', 'really-simple' ), 
-    'number'  => 10
-  ]); 
-?>
-  
+<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php get_template_part( 'template-parts/content' ); ?>
+    <?php endwhile; ?>
+
+    <?php the_posts_navigation(); ?>
+  <?php else: ?>
+    <?php echo '<p>' . esc_html__( 'No results were found, try searching for another word.', 'really-simple' ) . '</p>'; ?>
+
+    <?php 
+      get_search_form();
+
+      the_widget( 'WP_Widget_Recent_Posts', [
+        'title'   => esc_html__( 'Latest Posts', 'really-simple' ), 
+        'number'  => 10
+      ]); 
+    ?>
+<?php endif; ?>
